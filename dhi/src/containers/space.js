24f19a5a01871e1a517d186ab83d6a83e13f906a
Priyanka <Priyanka.mydesire@gmail.com>
2020-10-12 20:39:18 +0200

    Integration with map marker is done

--- dhi/src/containers/space.js
+++ dhi/src/containers/space.js
@@ -1,86 +1,28 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./Home.css";
 import "mapbox-gl/dist/mapbox-gl.css";
 import MapGL, { GeolocateControl } from "react-map-gl";
-// import { Button } from "@material-ui/core";
-import Mapapi from './Mapapi';
-
+import { Marker } from "react-map-gl";
+import PoolSharpIcon from "@material-ui/icons/PoolSharp";
 const MAPBOX_TOKEN =
   "pk.eyJ1IjoicHJpeWFua2FwcyIsImEiOiJja2cyYzFjY3MxZTc4MnlxZm92d2Y4M3poIn0.7Eb13DlMMMXb-_UnsMgcVg";
 
-const geolocateStyle = {
-  float: "left",
-  margin: "50px",
-  padding: "10px",
-};
+const apiURL =
+  "https://wfs-kbhkort.kk.dk/k101/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=k101:svoemmehaller_mv&outputFormat=json&SRSNAME=EPSG:4326";
 
-// const style = {
-//   padding: "10px",
-//   color: "#fff",
-//   cursor: "pointer",
-//   background: "#1978c8",
-//   borderRadius: "6px",
-// };
+const style = {};
 
-// function fetchArray() {
-//   fetch(
-//     `https://wfs-kbhkort.kk.dk/k101/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=k101:affaldskurve_puma&outputFormat=json&SRSNAME=EPSG:4326`
-//   )
-//     .then((response) => response.json())
-//     .then((data) => {
-//       getcoords(data.features);
-//       return data;
-//     });
-// }
+const markers = [];
 
-
-// fetchArray();
-
-// function getcoords(data) {
-//   const finaldata = [];
-//   const data1 = data.map((item) => {
-//     const data2 = item.geometry.coordinates.map((coords) => {
-//       const coordsarray = coords.map((data) => {
-//         finaldata.push({
-//           latitude: coords[0],
-//           longitude: coords[1],
-//         });
-//         return finaldata;
-//       });
-
-//       return coordsarray;
-//     });
-
-//     return data2;
-//   });
-
-//   return finaldata;
-// }
-
-// const mytestVar = fetchArray();
-// console.log(mytestVar);
-
-// const markers = [
-//   { latitude: 0, longitude: 0 },
-//   { latitude: 0.5, longitude: 0.5 },
-//   { latitude: 1.5, longitude: 1.5 },
-//   { latitude: 12.56810795478189, longitude: 55.68194599506729 },
-//   { latitude: 10.567083630841283, longitude: 45.681152213615626 },
-// ];
-
-// function onMarkerClick(event) {
-//   alert("You clicked on marker");
-//   console.log("You clicked on marker");
-//   event.stopPropagation();
-// }
+const geolocateStyle = {};
 
 function Space() {
   const [viewport, setViewport] = useState({
     width: "100%",
     height: 400,
-    latitude: 0,
-    longitude: 0,
-    zoom: 1,
+    latitude: 55.676098,
+    longitude: 12.568337,
+    zoom: 10,
     bearing: 0,
     pitch: 0,
     marker: true,
@@ -89,18 +31,42 @@ function Space() {
   const _onViewportChange = (viewport) =>
     setViewport({ ...viewport, transitionDuration: 3000 });
 
-  // const [position, setPosition] = useState({ longitude: 0, latitude: 0 });
+  const [position, setPosition] = useState({});
 
-  // const updatePosition = (position) =>
-  //   setPosition({ ...position, transitionDuration: 3000 });
+  async function fetchData() {
+    const response = await fetch(apiURL);
+    response
+      .json()
+      .then((response) => {
+        const data = response.features;
+        const data1 = data.map((item) => {
+          const data2 = item.geometry.coordinates.map((coords) => {
+            const coordsarray = coords.map((data) => {
+              markers.push({
+                latitude: coords[1],
+                longitude: coords[0],
+              });
 
-  // setPosition({...position, longitude: "userInput", latitude: "Input by user"});
+              return markers;
+            });
+            return coordsarray;
+          });
+
+          return data2;
+        });
+        setPosition(markers);
+        return data1;
+      })
+
+      .catch((err) => alert(err));
+  }
+
+  useEffect(() => {
+    fetchData();
+  });
 
   return (
     <>
-      {/* <Button onClick={() => fetchArray()}>Default click</Button> */}
-      {/* <Button onClick={() => this.setPosition({ ...this.position })}>Default click</Button> */}
-
       <div style={{ margin: "0 auto" }}>
         <h1
           style={{
@@ -112,6 +78,7 @@ function Space() {
           GeoLocator: Click To Find Your Location or click{" "}
           <a href="/search">here</a> to search for a location
         </h1>
+
         <MapGL
           {...viewport}
           mapboxApiAccessToken={MAPBOX_TOKEN}
@@ -123,24 +90,19 @@ function Space() {
             positionOptions={{ enableHighAccuracy: true }}
             trackUserLocation={true}
           />
-          {/* <Marker
-            longitude={markers[0].longitude}
-            latitude={markers[0].latitude}
-            onClick={onMarkerClick}
-            // onDragEnd={onDragEnd}
-            draggable
-          >
-            <div style={style}>
-              hi
-              {/* <span>🗑️</span> */}
-          {/* </div>
-          </Marker> */}
-
-           <Mapapi/>
+          {markers.map((location, index) => (
+            <Marker
+              latitude={location.latitude}
+              longitude={location.longitude}
+              key={index}
+              draggable
+            >
+              <div style={style}>
+                <PoolSharpIcon />
+              </div>
+            </Marker>
+          ))}
         </MapGL>
-        {/* {markers.map(marker => (
-          <div>{marker}</div>
-        ))} */}
       </div>
     </>
   );
--- dhi/src/containers/time.js
+++ dhi/src/containers/time.js
@@ -3,43 +3,50 @@ import "./Home.css";
 import { Box, Container } from "@material-ui/core";
 import { Button } from "@material-ui/core";
 import {
-  ComposedChart, Line, Area, Bar, XAxis, YAxis, CartesianGrid, Tooltip,
+  ComposedChart,
+  Line,
+  Area,
+  Bar,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
   Legend,
-} from 'recharts';
-import { makeStyles } from '@material-ui/core/styles';
-import TextField from '@material-ui/core/TextField';
+} from "recharts";
+import { makeStyles } from "@material-ui/core/styles";
+import TextField from "@material-ui/core/TextField";
 
-const apiURL = "https://api.met.no/weatherapi/locationforecast/2.0/complete?altitude=0&lat=55.6761&lon=12.5683";
+const apiURL =
+  "https://api.met.no/weatherapi/locationforecast/2.0/complete?altitude=0&lat=55.6761&lon=12.5683";
 
-const realdata = []
+const realdata = [];
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    '& > *': {
+    "& > *": {
       margin: theme.spacing(1),
-      width: '25ch',
+      width: "25ch",
     },
   },
 }));
 
 function Time() {
-
   const [Graphdata, setGraphdata] = useState({});
 
   async function fetchData() {
     const response = await fetch(apiURL);
     response
       .json()
-      .then(response => {
+      .then((response) => {
         const weathercycle = response.properties.timeseries;
-      weathercycle.map(item => {
-        const weatherdata = item.data.instant.details;
-        realdata.push(weatherdata)
-        return weatherdata;
-      });
-      setGraphdata(realdata);
+        weathercycle.map((item) => {
+          const weatherdata = item.data.instant.details;
+          realdata.push(weatherdata);
+          return weatherdata;
+        });
+        setGraphdata(realdata);
       })
-      .catch(err => alert(err));
+      .catch((err) => alert(err));
   }
   useEffect(() => {
     fetchData();
@@ -48,40 +55,67 @@ function Time() {
   const classes = useStyles();
 
   return (
-      <Container maxWidth="lg">
-        <h2>Time</h2>
-        <p>In this graph data is fetched acording to the location input by user.</p>
-        <Box component="div" >
+    <Container maxWidth="lg">
+      <h2>Time</h2>
+      <p>
+        In this graph data is fetched acording to the location input by user.
+      </p>
+      <Box component="div">
         <form className={classes.root} noValidate autoComplete="off">
-  
-  <TextField id="outlined-basic" label="City name" variant="outlined" size="small" /><br/>
-  <Button variant="contained" color="primary"  size="large" onClick={() => fetchData()}>
-  Submit
-</Button>
-</form>
-        
-        </Box>
-        <Box component="div" >
-  
-          <ComposedChart
+          <TextField
+            id="outlined-basic"
+            label="City name"
+            variant="outlined"
+            size="small"
+          />
+          <br />
+          <Button
+            variant="contained"
+            color="primary"
+            size="large"
+            onClick={() => fetchData()}
+          >
+            Submit
+          </Button>
+        </form>
+      </Box>
+      <Box component="div">
+        <ComposedChart
           width={1100}
           height={400}
           data={Graphdata}
           margin={{
-            top: 20, right: 80, bottom: 30, left: 20,
+            top: 20,
+            right: 80,
+            bottom: 30,
+            left: 20,
           }}
         >
           <CartesianGrid stroke="#f5f5f5" />
-          <XAxis dataKey="air_temperature" label={{ value: 'Air Temperature', position: 'insideBottomRight', offset: 0 }} />
-          <YAxis label={{ value: 'Index', angle: -90, position: 'insideLeft' }} />
+          <XAxis
+            dataKey="air_temperature"
+            label={{
+              value: "Air Temperature",
+              position: "insideBottomRight",
+              offset: 0,
+            }}
+          />
+          <YAxis
+            label={{ value: "Index", angle: -90, position: "insideLeft" }}
+          />
           <Tooltip />
           <Legend />
-          <Area type="monotone" dataKey="cloud_area_fraction" fill="#8884d8" stroke="#8884d8" />
+          <Area
+            type="monotone"
+            dataKey="cloud_area_fraction"
+            fill="#8884d8"
+            stroke="#8884d8"
+          />
           <Bar dataKey="cloud_area_fraction_low" barSize={20} fill="#413ea0" />
           <Line type="monotone" dataKey="relative_humidity" stroke="#ff7300" />
         </ComposedChart>
       </Box>
-      </Container>
+    </Container>
   );
 }
 
